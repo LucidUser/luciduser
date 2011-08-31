ba51a02894871a6aedf8125b9a5ac4445ebc5214
snewman205 <snewman205@gmail.com>
2011-08-31 17:35:34 -0400

    updated template.php [8/31/11 17:35:32 PM]

--- src/include/template.php
+++ src/include/template.php
@@ -9,9 +9,7 @@
       private $xtpl = null;
   
       function __construct($theme = '') {
-
-         // TODO: auto-detect theme from db if none is specified      
-
+      
          if(!empty($theme)) { $this->default_theme = $theme; } else { $this->default_theme = autodetect; }
          chdir("../");
          $this->theme_path = getcwd() . "/themes/" . $this->default_theme . "/";
@@ -19,9 +17,33 @@
       }
 
       function buildTemplate($params = '', $template) {
-    
+      
          $this->xtpl = new XTemplate($this->theme_path . $template);
          $this->xtpl->assign("THEME", $this->default_theme);
+         $tp = substr("../" . getRelativePath(getcwd(),$this->theme_path),0,-1);
+         $rp = substr("../" . getRelativePath(getcwd(),getcwd() . "/"),0,-1);
+         $this->xtpl->assign("THEME_PATH", $tp);
+         $this->xtpl->assign("SCRIPT_PATH", $rp);
+         $this->xtpl->assign("JS", "
+      <script type=\"text/javascript\" src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js\"></script>
+      <script type=\"text/javascript\" src=\"https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js\"></script>
+      <script type=\"text/javascript\" src=\"" . $rp . "/js/lib-ajax.js\"></script>
+      <script type=\"text/javascript\">
+     
+         var THEME = \"\"; var THEME_PATH = \"\"; var SCRIPT_PATH = \"\"; var ajax = \"\"; var notifier = \"\";
+     
+         $(document).ready(function() {
+         
+            window.THEME = \"" . $this->default_theme . "\";
+            window.THEME_PATH = \"" . $tp . "\";
+            window.SCRIPT_PATH = \"" . $rp . "\";
+            window.ajax = new Ajax();
+            window.notifier = new AjaxNotifier();
+
+         });
+
+      </script>");
+
    
          if(!empty($params)) {
    
